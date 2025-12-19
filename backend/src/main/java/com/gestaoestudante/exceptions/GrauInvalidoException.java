9c0de4bf5b439fca3e306a5b10140e5ae6c53e06
tarcilioMz <tarciliomuzime@gmail.com>
2025-12-19 16:28:57 +0200

    refactor: adiciona exception de dominio para Grau do docente [Mestrado, Licenciatura,etc]

---
+++ backend/src/main/java/com/gestaoestudante/exceptions/GrauInvalidoException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.gestaoestudante.exceptions;
+
+/**
+ *
+ * @author Muzime
+ */
+public class GrauInvalidoException extends RuntimeException{
+    public GrauInvalidoException(String mensagem){
+        super(mensagem);
+    }
+    
+}
