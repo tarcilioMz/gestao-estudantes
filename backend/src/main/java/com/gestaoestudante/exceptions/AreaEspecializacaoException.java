eee6980ed9189caf7a3f9b019246ca4823275ed4
tarcilioMz <tarciliomuzime@gmail.com>
2025-12-19 16:27:07 +0200

    refactor: adiciona execptions de dominio AreaDeExpecializacao do docente

---
+++ backend/src/main/java/com/gestaoestudante/exceptions/AreaEspecializacaoException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.gestaoestudante.exceptions;
+
+/**
+ *
+ * @author Muzime
+ */
+public class AreaEspecializacaoException extends RuntimeException{
+    public AreaEspecializacaoException(String mensagem){
+        super(mensagem);
+    }
+    
+}
